fdf5e77f18489fe56f57ad9492abd92efb7047fa
Joe Reddington <joe@equalitytime.co.uk>
2023-11-22 07:52:01 +0000

    Update

--- localdeploy.sh
+++ localdeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-cp /home/joe/snap/firefox/common/.mozilla/firefox/lgcolshm.default/places.sqlite databases/firefox.sqlite
+cp /home/joe/snap/firefox/common/.mozilla/firefox/92ur2814.default/places.sqlite databases/firefox.sqlite
 python3 export_history.py 
 cat _includes/head.html > _site/index.html
 cat _site/history.html >> _site/index.html
